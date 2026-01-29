1d44a9770a12fe3bcdf95d9f2ff13d13db788cae
singhjas <singhjas@msoe.edu>
2026-01-29 15:35:04 -0600

    Complete Activity 1

---
+++ activity-1/scripts.js
@@ -0,0 +1,30 @@
+console.log("Hello World!");   
+document.getElementById("output").innerHTML = "Hello, World!";
+
+let studentName = "Jasdeep Singh";
+const age = 25;
+let isStudent = true;
+let emptyValue = null;
+let notAssigned;
+
+console.log("Student Name:", studentName);
+console.log("Type of studentName:", typeof studentName);
+
+console.log("Age:", age);
+console.log("Type of age:", typeof age);
+
+console.log("Is Student:", isStudent);
+console.log("Type of isStudent:", typeof isStudent);
+
+console.log("Empty Value:", emptyValue);
+console.log("Type of emptyValue:", typeof emptyValue);
+
+console.log("Not Assigned:", notAssigned);
+console.log("Type of notAssigned:", typeof notAssigned);
+
+studentName = "Mike Ross";
+
+console.log("Updated Student Name:", studentName);
+
+age = 30;
+
